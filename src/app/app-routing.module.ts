efa15a3a1f6188fbd151b4c8e1ebd16fd8d062cb
jeremy <j.arias@motorcredito.com.do>
2023-06-14 16:08:50 -0400

    Adding Children in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404pageComponent } from './shared/pages/error404page/error404page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path:'404',
+    component:Error404pageComponent
+  },
+  {
+    path: '',
+    redirectTo:'heroes',
+    pathMatch: 'full'
+  },
+  {
+    path:'**',
+    redirectTo:'404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroePageComponent } from './pages/heroe-page/heroe-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',
+  component:LayoutPageComponent,
+  children:[
+    {path:'new-hero', component:NewPageComponent},
+    {path:'search', component:SearchPageComponent},
+    {path:'edit/:edit', component:NewPageComponent},
+    {path:'list', component:ListPageComponent},
+    {path:':id', component:HeroePageComponent},
+    {path:'**', redirectTo:'list'}
+  ]
+}
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
