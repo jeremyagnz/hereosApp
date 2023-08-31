0638a4da8d2a5d82a8c9bc52bafd897717be5236
jeremy <j.arias@motorcredito.com.do>
2023-08-31 18:58:10 -0400

    Adding heroe get request by id

--- src/app/heroes/pages/heroe-page/heroe-page.component.ts
+++ src/app/heroes/pages/heroe-page/heroe-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesServices } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-heroe-page',
   templateUrl: './heroe-page.component.html',
   styleUrls: ['./heroe-page.component.css']
 })
-export class HeroePageComponent {
+export class HeroePageComponent implements OnInit{
 
+  public hero?: Hero;
+  constructor(
+    private activatedRoute:ActivatedRoute,
+    private heroesServices:HeroesServices,
+    private router:Router
+    ){}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id})  => this.heroesServices.getHeroById(id)),
+      ).subscribe( hero => {
+
+        if(!hero) return this.router.navigate(['/heroes/list']);
+
+        this.hero = hero;
+        return;
+      })
+  }
 }
