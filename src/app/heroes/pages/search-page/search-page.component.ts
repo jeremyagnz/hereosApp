d19060fad792e1ed124043150215eed41ac95fef
jeremy <j.arias@motorcredito.com.do>
2023-09-01 02:48:05 -0400

    Creating Search

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/heroes.interface';
+import { HeroesServices } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -6,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent {
+
+public searchInput =  new FormControl('');
+public heroes: Hero[] = [];
+public selectedHero?:Hero;
+
+constructor(private heroesServices:HeroesServices){}
+
+searchHero(){
+  const value:string = this.searchInput.value || '';
+
+  this.heroesServices.getSuggestions( value )
+    .subscribe(heroes => this.heroes = heroes );
+}
+
+onSelectedOption(event: MatAutocompleteSelectedEvent):void{
+  if(!event.option.value){
+    this.selectedHero = undefined;
+    return;
+  }
+
+  const hero:Hero = event.option.value;
+  this.searchInput.setValue( hero.superhero);
+
+  this.selectedHero = hero;
+}
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -24,4 +24,8 @@ export class HeroesServices {
     );
   }
 
+  getSuggestions( query:string ):Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${ query }&_limit=6`);
+  }
+
 }
